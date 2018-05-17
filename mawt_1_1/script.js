7c5620e189df8cd6983d455cef779a2f4f4ab1e8
Manuel <mlsanchez77@gmail.com>
2018-05-17 13:07:54 +0200

    Commit Intercionacionalización

--- mawt_1_1/script.js
+++
@@ -1,7 +0,0 @@
-//Este punto y coma que se pone es una buena práctica para evitar conflictos
-;
-console.log('hola');
-
-//Para evitar conflictos con versiones de JQuery (usado por WP) entre WP y sus plugins
-
-//$ = Jquery.noConflict();
--- mawt_1_2/404.php
+++
@@ -1,9 +0,0 @@
-<?php
-get_header();
-echo '<h3><spam class="err404">Hola Perra error 404</spam></h3>';
-get_sidebar();
-get_footer();
-
-
-
-
